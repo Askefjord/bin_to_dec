6871b431a8921a3e749a4b08e2f8c20a00a99e73
Askefjord <kriwjar_gubicky@icloud.com>
2024-11-27 00:35:43 +0300

    dev transfer algorithm

--- main.py
+++ main.py
@@ -1,1 +1,8 @@
-print(bin(12))
+x = int(input('Which system should I transfer to?\n1. to DEC\n2. to BIN\n'))
+y = input('Enter number: ')
+
+if x==1:
+    print(int(y, 2))
+else:
+    y=int(y)
+    print(bin(y))
