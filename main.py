b02a04ed8ace9f047ce1a13ac1f61b72259c7117
Askefjord <kriwjar_gubicky@icloud.com>
2024-11-28 22:12:08 +0300

    вdebug compile erros

--- main.py
+++ main.py
@@ -1,8 +1,25 @@
-x = int(input('Which system should I transfer to?\n1. to DEC\n2. to BIN\n'))
-y = input('Enter number: ')
+isStart = True
+while isStart == True:
+    try:
+        x = int(input('Which system should I transfer to?\n'
+            '1. to DEC\n'
+            '2. to BIN\n'
+            '0. Exit\n'))
 
-if x==1:
-    print(int(y, 2))
-else:
-    y=int(y)
-    print(bin(y))
+        if x==1:
+            y = input('Enter number: ')
+            print(int(y, 2))
+        elif x==2:
+            y = input('Enter number: ')
+            y=int(y)
+            print(bin(y))
+        elif x==0:
+            isStart = False
+            break
+        else:
+            print('Invalid number')
+            break
+
+    except ValueError:
+        print('Error\n')
+        break
